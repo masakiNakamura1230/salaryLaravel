131d8bf75c1983ba7871db81fdde06bb8688e48f
masakiNakamura <106379929+masakiNakamura1230@users.noreply.github.com>
2022-07-14 14:09:49 +0000

    [Add] タレント一覧/給与一覧変更

--- app/Models/Salary.php
+++ app/Models/Salary.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
 class Salary extends Model
@@ -15,16 +16,36 @@ class Salary extends Model
      * 稼働日の日付フォーマット
      * @return string
      */
-    public function getWorkingDateAttribute(){
-        // キャメルケースでメソッド名を指定してスネークケースでテンプレートで表示
-        return Carbon::parse($this->attributes['working_date'])->format('Y年m月d日');
-    }
+    // public function getWorkingDateAttribute(){
+    //     // キャメルケースでメソッド名を指定してスネークケースでテンプレートで表示
+    //     return Carbon::parse($this->attributes['working_date'])->format('Y年m月d日');
+    // }
 
     /**
      * 更新日の日付フォーマット
      * @return string
      */
-    public function getUpdatedAtAttribute(){
-        return Carbon::parse($this->attributes['updated_at'])->format('Y年m月d日');
+    // public function getUpdatedAtAttribute(){
+    //     return Carbon::parse($this->attributes['updated_at'])->format('Y年m月d日');
+    // }
+
+    // 給与一覧取得
+    public function getSalarySelect(int $i){
+        return $salaries = DB::table('salaries as s')
+        ->select([
+            's.id as id',
+            's.talent_id as talent_id',
+            't.name as talentName',
+            's.manager_id as manager_id',
+            'm.name as managerName',
+            's.work',
+            's.salary',
+        ])
+        ->selectRaw('DATE_FORMAT(working_date, "%Y年%m月%d日") as working_date')
+        ->selectRaw('DATE_FORMAT(updated_at, "%Y年%m月%d日") as updated_at')
+        ->join('talents as t', 's.talent_id', '=', 't.id')
+        ->join('managers as m', 's.manager_id', '=', 'm.id')
+        ->where('s.talent_id', '=', $i)
+        ->get();
     }
 }
